96c3616b385fbd87b53eeda69ccc552879a202c9
egesencan <egesencan@gmail.com>
2019-12-02 17:26:09 +0100

    init

---
+++ product-descp-service/product_descp.js
@@ -0,0 +1,21 @@
+module.exports = function (options) {
+    //Import the mock data json file
+    const mockData = require('./MOCK_DATA.json');
+
+    //Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductURL', productURL);
+    this.add('role:product,cmd:getProductName', productName);
+
+
+    //To DO: add the pattern functions and describe the logic inside the function
+    function productURL(msg, respond) {
+            respond(null, { result: ''});
+        
+    }
+
+    function productName(msg, respond) {
+        
+            respond(null, { result: ''});
+        
+    }
+}
